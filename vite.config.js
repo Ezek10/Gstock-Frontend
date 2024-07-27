d6c87d3bdd14c98b471dea2757dc1aea7abea92b
Ezek10 <ezequielmarcel2@gmail.com>
2024-07-26 21:09:31 -0300

    add dockerfile

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port: 80, // Este es el puerto que usaremos en Docker
+    watch: {
+      usePolling: true, // Agrega esto si estás en Windows y el hot reload no funciona correctamente
+    },
+  },
 })
