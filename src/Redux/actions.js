ad94df83deded4c89331e4f371f47ad40313ff72
Ezek10 <ezequielmarcel2@gmail.com>
2025-01-17 20:54:59 -0300

    fix remove due access token

--- src/Redux/actions.js
+++ src/Redux/actions.js
@@ -71,6 +71,7 @@ const getHeaders = () => ({
 
 const handleUnauthorizedError = (error) => {
   if (error.response && error.response.status === 401) {
+    localStorage.removeItem("access_token")
     window.location.href = `${USER_URL}/refresh_token`;
   }
 };
